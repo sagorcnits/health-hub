839ae482760884f6ff412886fcf3a08065caa97d
Sagor Hossain <sagor.cnits@gmail.com>
2024-07-11 18:51:57 +0600

    doctor card added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,40 @@
 import HomeBanner from "@/components/HomeBanner";
+import DoctorCard from "@/components/homeComponents/DoctorCard";
 import OurService from "@/components/homeComponents/OurService";
 import ReviewSlider from "@/components/homeComponents/ReviewSlider";
 import ServiceCard from "@/components/homeComponents/ServiceCard";
 import SectionIntro from "@/components/SectionIntro";
-
+import dotctor1 from "../images/doctor-1.png";
+import dotctor2 from "../images/doctor-2.png";
+import dotctor3 from "../images/doctor-3.png";
 export default function Home() {
+  const doctorInfo = [
+    {
+      image: dotctor1,
+      name: "Karyen Anderson",
+      proffesion: "BTP -  Senior Physiotherapist",
+      address: "Dhanmondi, Dhaka, Bangladesh",
+      available: "Available On Mon, 22 December",
+      fee: 15,
+    },
+    {
+      image: dotctor2,
+      name: "Karyen Anderson",
+      proffesion: "BTP -  Senior Physiotherapist",
+      address: "Dhanmondi, Dhaka, Bangladesh",
+      available: "Available On Mon, 22 December",
+      fee: 25,
+    },
+    {
+      image: dotctor3,
+      name: "Karyen Anderson",
+      proffesion: "BTP -  Senior Physiotherapist",
+      address: "Dhanmondi, Dhaka, Bangladesh",
+      available: "Available On Mon, 22 December",
+      fee: 15,
+    },
+  ];
+
   return (
     <>
       <HomeBanner></HomeBanner>
@@ -22,6 +52,20 @@ export default function Home() {
         ></SectionIntro>
         <div className="max-w-7xl mx-auto mt-16">
           <ReviewSlider></ReviewSlider>
+        </div>
+      </section>
+      <section className="mt-16 max-w-7xl mx-auto">
+        <SectionIntro
+          info={{
+            header: "Our Expert Doctors",
+            title:
+              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+          }}
+        ></SectionIntro>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center mt-10">
+          {doctorInfo?.map((doctor, id) => (
+            <DoctorCard doctorInfo={doctor}></DoctorCard>
+          ))}
         </div>
       </section>
     </>
---
+++ src/components/homeComponents/DoctorCard.jsx
@@ -0,0 +1,106 @@
+import Image from "next/image";
+const DoctorCard = ({doctorInfo}) => {
+  return (
+    <div className="card border border-b p-4 font-Source_Sans hover:card-effect duration-500">
+      <figure className="bg-[#DDDFE0] rounded-md overflow-hidden">
+        <Image className="w-full"  src={doctorInfo.image}></Image>
+      </figure>
+      <div>
+        <h3 className="text-[20px] font-Source_Sans py-2 font-semibold">{doctorInfo.name}</h3>
+        <p>{doctorInfo.proffesion}</p>
+        <div className="rating py-4 border-b border-[#F3F3F3] w-full">
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+            defaultChecked
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
+        </div>
+        <div className="flex items-center gap-4 py-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M15 10.5C15 11.2956 14.6839 12.0587 14.1213 12.6213C13.5587 13.1839 12.7956 13.5 12 13.5C11.2044 13.5 10.4413 13.1839 9.87868 12.6213C9.31607 12.0587 9 11.2956 9 10.5C9 9.70435 9.31607 8.94129 9.87868 8.37868C10.4413 7.81607 11.2044 7.5 12 7.5C12.7956 7.5 13.5587 7.81607 14.1213 8.37868C14.6839 8.94129 15 9.70435 15 10.5V10.5Z"
+              stroke="#6C6B6B"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M19.5 10.5C19.5 17.642 12 21.75 12 21.75C12 21.75 4.5 17.642 4.5 10.5C4.5 8.51088 5.29018 6.60322 6.6967 5.1967C8.10322 3.79018 10.0109 3 12 3C13.9891 3 15.8968 3.79018 17.3033 5.1967C18.7098 6.60322 19.5 8.51088 19.5 10.5V10.5Z"
+              stroke="#6C6B6B"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <p>{doctorInfo.address}</p>
+        </div>
+        <div className="flex items-center gap-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M6.75 3V5.25M17.25 3V5.25M3 18.75V7.5C3 6.90326 3.23705 6.33097 3.65901 5.90901C4.08097 5.48705 4.65326 5.25 5.25 5.25H18.75C19.3467 5.25 19.919 5.48705 20.341 5.90901C20.7629 6.33097 21 6.90326 21 7.5V18.75M3 18.75C3 19.3467 3.23705 19.919 3.65901 20.341C4.08097 20.7629 4.65326 21 5.25 21H18.75C19.3467 21 19.919 20.7629 20.341 20.341C20.7629 19.919 21 19.3467 21 18.75M3 18.75V11.25C3 10.6533 3.23705 10.081 3.65901 9.65901C4.08097 9.23705 4.65326 9 5.25 9H18.75C19.3467 9 19.919 9.23705 20.341 9.65901C20.7629 10.081 21 10.6533 21 11.25V18.75"
+              stroke="#6C6B6B"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <p>{doctorInfo.available}</p>
+        </div>
+        <div className="flex items-center gap-4 py-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M12 6V18M9 15.182L9.879 15.841C11.05 16.72 12.949 16.72 14.121 15.841C15.293 14.962 15.293 13.538 14.121 12.659C13.536 12.219 12.768 12 12 12C11.275 12 10.55 11.78 9.997 11.341C8.891 10.462 8.891 9.038 9.997 8.159C11.103 7.28 12.897 7.28 14.003 8.159L14.418 8.489M21 12C21 13.1819 20.7672 14.3522 20.3149 15.4442C19.8626 16.5361 19.1997 17.5282 18.364 18.364C17.5282 19.1997 16.5361 19.8626 15.4442 20.3149C14.3522 20.7672 13.1819 21 12 21C10.8181 21 9.64778 20.7672 8.55585 20.3149C7.46392 19.8626 6.47177 19.1997 5.63604 18.364C4.80031 17.5282 4.13738 16.5361 3.68508 15.4442C3.23279 14.3522 3 13.1819 3 12C3 9.61305 3.94821 7.32387 5.63604 5.63604C7.32387 3.94821 9.61305 3 12 3C14.3869 3 16.6761 3.94821 18.364 5.63604C20.0518 7.32387 21 9.61305 21 12Z"
+              stroke="#6C6B6B"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <p>${doctorInfo.fee}</p>
+        </div>
+        <button className="button-2 w-full font-semibold text-[20px]">View Profile</button>
+      </div>
+    </div>
+  );
+};
+
+export default DoctorCard;
